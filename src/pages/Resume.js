76f227d051a61444816902d6d150986702d98ee1
jay <jayleungg@gmail.com>
2022-09-16 18:27:34 -0400

    resume page up

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,7 +1,158 @@
 import React from "react";
 
 const Resume = () => {
-  return <div>Resume</div>;
+  return (
+    <div>
+      <div className="tee"></div>
+      <body className="resume-body">
+        <h1>Jay Leung</h1>
+        <h2>
+          <b>Full Stack Developer</b>
+        </h2>
+        <p>
+          <a href="tel:1+347-605-3611">347-605-3611</a> |{"   "}
+          <a href="mailto:jayleungg@gmail.com"> JayLeungg@gmail.com</a> |
+          {"    "}
+          <span>
+            <a href="https://github.com/jayl2" target="_blank">
+              GitHub
+            </a>
+          </span>{" "}
+        </p>
+        <p>
+          I am a skilled software developer. Infused with a high level of
+          attention to detail, identifying problems at ease with a strong
+          ability to problem solve. I carry a diverse set of skills including
+          leadership, coaching, engagement, strategic planning and strong
+          ability to prioritize.
+        </p>
+        <p>
+          <b>QUALIFICATIONS</b>
+        </p>
+        <p>
+          Management Proficiency | Client relationships | | Exceptional
+          Organizational Skills | Collaborative with the ability to prioritize
+          and meet deadlines | Time Management | Strategic Planning | Engagement
+          | Training & Development | Project Management | Conflict Resolution |
+          High Level Attention To Detail
+        </p>
+        <p>
+          <b> Technical Skill Set</b>
+        </p>
+        <p>
+          HTML5 | CSS3 | Javascript ES6 | jQuery | ReactJS | Node.JS | MongoDB |
+          Mongoose |MERN |Git| GitHub | Python3 |VueJS |Django
+        </p>
+        <p>
+          <b> EXPERIENCES</b>
+        </p>
+        <h5>General Assembly - 2022</h5>
+        <h6>Software Engineer Immersive</h6>
+        <li>
+          Over 500 hours deeli diving into modern software development
+          technologies. Implementing today’s languages and frameworks to build
+          various projects.
+        </li>
+        <li>
+          Children’s Farm Adventure - Created a children’s interactive quiz game
+          with pure Javascript, HTML and CSS.
+        </li>
+        <li>
+          {" "}
+          Gobble Restaurant Reviews FullStack application to enable full CRUD w/
+          MERN Stack for users to Input Restaurant reviews.{" "}
+        </li>
+        <li>
+          Quizards - Full PERN Stack to provide CRUD operations for students to
+          study topics/subjects or their desire and/or curriculum.
+        </li>
+        <li>
+          {" "}
+          PhotoShopR - A Full MERN eCommerce stack with full CRUD operations
+          where users can shop Digital Cameras and Lenses.
+        </li>
+        <h5>CVS Health - District Training Manager / Coach 2016 - 2022</h5>
+        <h6>Foundational Leader</h6>
+        <li>Oversee top volume store in lower Manhattan Market - $11M+</li>
+        <li>
+          2020 Top District Overall OSAT Service Achievement Award exceed target
+          by 11 pts- CVS{" "}
+        </li>
+        <li>2017, 2018, 2020 District Paragon Award Winner - CVS</li>
+        <li>
+          Assist in overseeing and orchestrating company COVID-19 testing sites
+          rollout by scoping for real estate to establish test sites.
+        </li>
+        <li>
+          Led NY Metro Market in Flu and expanded immunization to top 5 results
+          chain wide, exceeding target 152%
+        </li>
+        <li>
+          Oversee and train new Store Managers and District Managers of various
+          calibers.
+        </li>
+        <li>
+          Oversee and responsible for a staff of 50-60 Colleagues including 10
+          Managers
+        </li>
+        <li>
+          District ‘MySchedule’ Expert - Assist and aid in scheduling and
+          staffing opportunities.{" "}
+        </li>
+        <li>
+          Conduct hiring events for a district of 14 stores in a competitive
+          market.
+        </li>
+        <li>
+          Ensure the stores comply with outlined safety policies and procedures,
+          in accordance with state and local regulations.
+        </li>
+        <li>
+          Facilitate Training sessions for 14 stores, 28 leaders and +150
+          colleagues on a quarterly basis.
+        </li>
+        <h5>
+          Rite Aid Corporation - Store Manager / Regional Trainer 2005- 2016
+        </h5>
+        <h6>Emerging Leader</h6>
+        <li>
+          Conduct training and oversee newly hired / promoted management
+          positions of all store levels.
+        </li>
+        <li>District Center of Excellence” 3 consecutive years - Rite Aid</li>
+        <li>
+          Maintains store staff by recruiting, selecting, orienting and training
+          employees.
+        </li>
+        <li>
+          Maintains store staff results by coaching, counseling, and coaching
+          employees; planning, monitoring and appraising job results.
+        </li>
+        <li>
+          Completes store productivity requirements by scheduling and assigning
+          employees; following up on work results.
+        </li>
+        <li>
+          Maintains operations by initiating, coordinating, and enforcing
+          program, operational, and personnel policies and procedures.
+        </li>
+        <li>
+          Coordinated the flow of all operations ensuring proper execution.
+        </li>
+        <li>
+          Prepared weekly staff schedules, invoices, item recall and additional
+          administrative work.
+        </li>
+        <h5> EDUCATION</h5>
+        <h6>
+          LaGuardia Community College, CUNY - Business Management |Business
+        </h6>
+        <h6>
+          Administration General Assembly - Software Engineering Immersive
+        </h6>
+      </body>
+    </div>
+  );
 };
 
 export default Resume;

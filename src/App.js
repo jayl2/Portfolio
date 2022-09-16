ef95ad9d5c739de3d87dd91c4a410a8980503564
jay <jayleungg@gmail.com>
2022-09-16 10:10:54 -0400

    2nd run

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return <div className="App">Hello World! This is a test</div>;
 }
 
 export default App;

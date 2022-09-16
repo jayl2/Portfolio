7ca7d94ef0cbfcba1a87ba935e3c65be2b0779e7
jay <jayleungg@gmail.com>
2022-09-16 15:20:22 -0400

    starting projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { Col, Row, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div>
+      Projects
+      <Container>
+        <Row>
+          <h1> Carson's Farm Adventure</h1>
+        </Row>
+        <Row>
+          <h1>Gobble Resstaurant Reviews</h1>
+        </Row>
+        <Row>
+          <h1>Quizards Quiz Cards</h1>
+        </Row>
+        <Row>
+          <h1>PhotoShopR</h1>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Projects;

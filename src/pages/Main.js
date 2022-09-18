239da9223cfb71da90543c0cae1120ba4b4a3f36
Jay <jayleungg@gmail.com>
2022-09-17 22:45:08 -0400

    updated projects page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
-import { Routes, Route } from "react-router-dom";
+
 
 const Main = () => {
   return (
@@ -23,21 +22,22 @@ const Main = () => {
         </div>
       </header>
       <div className="second-div">
-        <Row>
+        
           <h2>Skill Set</h2>
           <h4>
             HTML5 | CSS 3 | ES6 | ReactJS | NODE.JS | MongoDB | Git | Python |
             Django | Vue
           </h4>
-        </Row>
-        <Row>
-          <Col>
+        
+          <div>
             <img
               className="icons"
               src="https://cdn-icons-png.flaticon.com/512/3074/3074119.png"
               alt="React"
             ></img>
+            </div>
 
+            <div>
             <img
               className="icons"
               src="https://cdn-icons-png.flaticon.com/512/29/29104.png"
@@ -53,13 +53,14 @@ const Main = () => {
               src="https://cdn-icons-png.flaticon.com/512/721/721791.png"
               alt="Javascript"
             />
-          </Col>
-          <Col>
+         
             <img
               className="icons"
               src="https://cdn-icons-png.flaticon.com/512/919/919825.png"
               alt="Node"
             />
+        </div>
+        <div>
             <img
               className="icons"
               src="https://cdn-icons-png.flaticon.com/512/31/31714.png"
@@ -75,8 +76,8 @@ const Main = () => {
               src="https://cdn-icons-png.flaticon.com/512/1822/1822920.png"
               alt="Python"
             />
-          </Col>
-          <Col>
+          
+          
             <img
               className="icons"
               src="https://seekicon.com/free-icon-download/django_2.svg"
@@ -87,8 +88,9 @@ const Main = () => {
               src="https://seekicon.com/free-icon-download/vue_1.svg"
               alt="Vue"
             />
-          </Col>
-        </Row>
+          </div>
+
+        
       </div>
     </div>
   );
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,22 +5,28 @@ import { Link } from "react-router-dom";
 const Projects = () => {
   return (
     <div>
-      Projects
-      <Container>
+      <h1> My Projects</h1>
+      <Container className='w3-animate-opacity'>
         <Row>
           <Col > 
           <h1>Carson's Farm Adventure</h1>
-          <img className='farm' src='https://i.imgur.com/1PpJgTf.png'></img>
-          <img className='farm'src='https://i.imgur.com/kjCJZUE.png'></img>
+          <img className='project-image' src='https://i.imgur.com/1PpJgTf.png'></img>
+          <h4>Javascript</h4>
+          <p> Carson's Farm  is a children's interactive game using pure Vanilla Javascript.</p>
+         
           </Col>
        
           <Col>
-          <h1>Gobble Resstaurant Reviews</h1>
+          <h1>Gobble Restaurant Reviews</h1>
+          <img className='project-image' src='https://i.imgur.com/YMY6L3d.png'/>
+          <h4> MERN Stack</h4>
           </Col>
         </Row>
         <Row>
           <Col>
           <h1>Quizards Quiz Cards</h1>
+          <img className = 'project-image' src='https://i.imgur.com/D7ytHbA.png'/>
+          <h4>PERN Stack</h4>
           </Col>
           <Col>
           <h1>PhotoShopR</h1>

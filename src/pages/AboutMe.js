b303e7a0235202400b61074a4a4018ca337b6db7
Jay <jayleungg@gmail.com>
2022-09-18 20:17:43 -0400

    test7

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const AboutMe = () => {
-  return <div>Jay Leung</div>;
+  return <div>Jay Leung
+
+<header className='about-header'>
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+</header>
+    
+<body className='about-body'>
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+</body>
+<div className='about-bottom'>sdfasdf</div>
+  </div>;
 };
 
 export default AboutMe;

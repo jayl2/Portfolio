fd62a7f27cc0f1a4e787c46a3ed66a23cd1b69f6
Jay <jayleungg@gmail.com>
2022-09-18 20:01:20 -0400

    test6

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -12,7 +12,9 @@ const Projects = () => {
           <h3>Carson's Farm Adventures</h3>
           <img className='project-image' src='https://i.imgur.com/1PpJgTf.png'></img>
           <h4>Javascript</h4>
-          <p> Carson's Farm is a children's interactive game using pure Vanilla Javascript.</p>
+          <p> Carson's Farm is a children's interactive game built with HTML, CSS and Vanilla Javascript. 
+            It is a game to build children's understanding of the Alphabet. 
+          </p>
           <Button variant='primary'>View Page</Button>
          
           </Col>
@@ -32,13 +34,18 @@ const Projects = () => {
           <h3>Quizards Quiz Cards</h3>
           <img className = 'project-image' src='https://i.imgur.com/D7ytHbA.png'/>
           <h4>PERN Stack</h4>
-          <p>Quizards is study tool</p>
+          <p>Quizards is study tool where students are able to create flash cards to study. 
+            Users able to sign up and log in to create unique subjects and questions around the area of study. 
+          </p>
           <Button variant='primary'>View Page</Button>
           </Col>
           <Col className='project-container' >
           <h3>PhotoShopR</h3>
           <img className='project-image' src='https://i.imgur.com/m1zBc1W.png'/>
           <h4>MERN Stack w/ Redux & JWT</h4>
+          <p>An eCommerce website for users to shop for photograpgy items. This project was created with 
+            Postgres for the backend, React for the front end with Redux to manage state and JWT for authentication. 
+          </p>
           <Button variant='primary'>View Page</Button>
           </Col>
         </Row>

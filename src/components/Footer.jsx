a595194e979ee81c8005f1e1720333c16ed4d232
jay <jayleungg@gmail.com>
2022-09-20 14:08:55 -0400

    footer links up

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,21 +4,27 @@ const Footer = () => {
   return (
     <div className="footer">
       <div>
-        <img
-          className="footer-icon"
-          src="https://seekicon.com/free-icon-download/github_18.svg"
-          alt="git"
-        />
-        <img
-          className="footer-icon"
-          src="https://seekicon.com/free-icon-download/linkedin-circle_1.svg"
-          alt="git"
-        />
-        <img
-          className="footer-icon"
-          src="https://seekicon.com/free-icon-download/mail-outline_1.svg"
-          alt="git"
-        />
+        <a href="https://github.com/jayl2" target="_blank">
+          <img
+            className="footer-icon"
+            src="https://seekicon.com/free-icon-download/github_18.svg"
+            alt="git"
+          />
+        </a>
+        <a href="http://www.linkedin.com/in/jay-leung1" target="_blank">
+          <img
+            className="footer-icon"
+            src="https://seekicon.com/free-icon-download/linkedin-circle_1.svg"
+            alt="LinkedIn"
+          />
+        </a>
+        <a href="mailto:jayleungg@gmail.com" target="_blank">
+          <img
+            className="footer-icon"
+            src="https://seekicon.com/free-icon-download/mail-outline_1.svg"
+            alt="git"
+          ></img>
+        </a>
       </div>
     </div>
   );
